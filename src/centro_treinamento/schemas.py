249b769e26eaf971a84f06e9cd8bd2ab293441aa
EyzRyder <gabriel.bessi.gb@gmail.com>
2024-07-01 20:02:34 -0300

    feat: add centro de treinamento controller

--- src/centro_treinamento/schemas.py
+++ src/centro_treinamento/schemas.py
@@ -7,7 +7,7 @@ from src.contrib.schemas import BaseSchema
 class CentroTreinamentoIn(BaseSchema):
     nome: Annotated[str, Field(description='Nome do centro de treinamento', examples=['CT King'], max_length=20)]
     endereco: Annotated[str, Field(description='Endereço do centro de treinamento', examples=['Rua X, Q02'], max_length=60)]
-    proprietario: Annotated[str, Field(description='Proprietario do centro de treinamento', examples=['Marcos']a, max_length=30)]
+    proprietario: Annotated[str, Field(description='Proprietario do centro de treinamento', examples=['Marcos'], max_length=30)]
 
 
 class CentroTreinamentoAtleta(BaseSchema):
